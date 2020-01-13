bc98faf0b48670e85e615acd4cb6e97c81995e96
steve10live <38049797+steve10live@users.noreply.github.com>
2020-01-13 23:22:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,8 +11,9 @@ chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 
+stamp1 = int(datetime.now().timestamp())
 driver.get("https://www.domofond.ru/1-komnatnaya-kvartira-v-arendu-moskva-2085427441")
-
+#driver.find_element_by_class_name('sc-cJSrbW iElBmz').click()
 for i in driver.find_elements_by_tag_name('button'):
     if 'showNumber' in i.get_attribute('class'):
         i.click()
@@ -21,3 +22,5 @@ search = re.search('tel:(.+?)"', str(driver.page_source))
 if search:
     print(search.group(1))
 driver.close()
+stamp2 = int(datetime.now().timestamp())
+print(stamp2 - stamp1)
