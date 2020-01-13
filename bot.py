74e5a140de4fd959da9c87c26c268460f8b3f0f1
steve10live <38049797+steve10live@users.noreply.github.com>
2020-01-13 23:19:24 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from time import sleep
 import os
+import re
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
@@ -7,5 +9,15 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-driver.get('https://www.google.com')
-print(driver.page_source)
+
+
+driver.get("https://www.domofond.ru/1-komnatnaya-kvartira-v-arendu-moskva-2085427441")
+
+for i in driver.find_elements_by_tag_name('button'):
+    if 'showNumber' in i.get_attribute('class'):
+        i.click()
+sleep(2)
+search = re.search('tel:(.+?)"', str(driver.page_source))
+if search:
+    print(search.group(1))
+driver.close()
