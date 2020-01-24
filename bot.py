fd7274cc439b45ae31dad0df80f0d83ab2c44361
steve10live <38049797+steve10live@users.noreply.github.com>
2020-01-24 16:05:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,9 +7,6 @@ from selenium import webdriver
 from time import sleep
 from datetime import datetime
 import os
-#from PIL import Image
-#import pytesseract
-
 
 def executive(new, logs):
     search = re.search('<function (\S+)', str(new))
@@ -26,8 +23,10 @@ def executive(new, logs):
 
 #pytesseract.pytesseract.tesseract_cmd = r'D:\Program Files (x86)\Tesseract-OCR\tesseract'
 try:
-    #text = str(pytesseract.image_to_string(Image.open('загружено.png'))) + '\n'
-    text = ''
+    from PIL import Image
+    import pytesseract
+    text = str(pytesseract.image_to_string(Image.open('загружено.png'))) + '\n'
+    #text = ''
     token = '617835554:AAHTqC39hgIGOSvaGEqrr8wDCGArB5EZwpA'
     bot = telebot.TeleBot(token)
     chrome_options = webdriver.ChromeOptions()
