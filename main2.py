fe48d353cd067b5bf774a8630b1a8d4141110392
steve10live <38049797+steve10live@users.noreply.github.com>
2020-01-31 22:26:42 +0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,8 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'}
+headers = {
+  'accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+  'accept-encoding':'gzip, deflate, br',
+  'accept-language':'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+  'cache-control':'no-cache',
+  'dnt': '1',
+  'pragma': 'no-cache',
+  'sec-fetch-mode': 'navigate',
+  'sec-fetch-site': 'none',
+  'sec-fetch-user': '?1',
+  'upgrade-insecure-requests': '1',
+  'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
+
+session = requests.Session()
+session.headers = headers
 print(headers)
-text = requests.get('https://www.avito.ru/moskva/kvartiry/sdam/na_dlitelnyy_srok?user=1', headers=headers)
+text = session.get('https://www.avito.ru/moskva/kvartiry/sdam/na_dlitelnyy_srok?user=1')
 print(text)
 soup = BeautifulSoup(text.text, 'html.parser')
 posts_raw = soup.find_all('div', class_='item__line')
