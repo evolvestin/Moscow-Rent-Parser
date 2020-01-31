261344f50a7b74cac7a09544f158ae8ec5ff0c79
steve10live <38049797+steve10live@users.noreply.github.com>
2020-01-31 22:18:30 +0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -2,7 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36'
                          ' (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36'}
+print(headers)
 text = requests.get('https://www.avito.ru/moskva/kvartiry/sdam/na_dlitelnyy_srok?user=1', headers=headers)
+print(text)
 soup = BeautifulSoup(text.text, 'html.parser')
 posts_raw = soup.find_all('div', class_='item__line')
 posts = []
